193d13d8c1475aaacff20ea5b9962ba4096cd7d4
AlijanHussain <6054499@mborijnland.nl>
2026-09-25 11:40:18 +0200

    kleine aanpassing
    minimale aanpassing

--- home.php
+++ home.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mbo soccer</title>
+    <title>Mbo Soccer</title>
     <link rel="stylesheet" href="css/style.css">
     <script src="javascript/java.js" defer></script>   
 </head>
